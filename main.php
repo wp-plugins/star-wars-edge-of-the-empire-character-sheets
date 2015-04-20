6bb518d8b08f4ccf77b239921bf3da9dd317e4eb
Davell87 <Davell87@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-20 20:14:40 +0000

    Final Version 1.5 for Basic Inventory Control
    git-svn-id: https://plugins.svn.wordpress.org/star-wars-edge-of-the-empire-character-sheets/trunk@1139639 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: Create and maintain your SW: Edge of the empire characters. Mobile Friendly.
  * Author: David Ellenburg
- * Version: 1.1.1
+ * Version: 1.5
  * Author URI: http://www.ellenburgweb.com
  * License: GPL2
  **/
@@ -98,13 +98,14 @@ add_shortcode( 'sweecs', 'sweecs_page');
 
 //data base insert info
 global $sweecs_db_version;
-$sweecs_db_version = '1.1';
+$sweecs_db_version = '1.2';
 function sweecs_install() {
 	global $wpdb;
 	global $sweecs_db_version;
-	$character_table_name = $wpdb->prefix . 'sweecs_characters';
-	$careers_table_name = $wpdb->prefix . 'sweecs_careers';
-	$skills_table_name = $wpdb->prefix . 'sweecs_skills';
+	$character_table_name=              $wpdb->prefix . 'sweecs_characters';
+	$character_inventory_table_name=    $wpdb->prefix . 'sweecs_characters_inventory';
+	$careers_table_name=                $wpdb->prefix . 'sweecs_careers';
+	$skills_table_name=                 $wpdb->prefix . 'sweecs_skills';
 
 	$charset_collate = $wpdb->get_charset_collate();
 	$sql = "CREATE TABLE $character_table_name (
@@ -163,6 +164,15 @@ function sweecs_install() {
 		UNIQUE KEY id (id)
 	) $charset_collate;";
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+	//Character inventory and Credits
+	$sql = "CREATE TABLE $character_inventory_table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		char_id text NOT NULL,
+		Credits int,
+		Inventory text,
+		UNIQUE KEY id (id)
+	)$charset_collate;";
 	dbDelta( $sql );
 	//Version 1.1.1 added Careers
 	$sql = "CREATE TABLE $careers_table_name (
